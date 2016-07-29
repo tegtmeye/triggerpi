83417e6dad8ca186b8c11f3f22c5f76424eb00dd
Mike Tegtmeyer <tegtmeye@gmail.com>
2016-07-29 16:40:49 -0400

    Broke out SPI configuration commands

--- src/triggerpi/board_init.cc
+++ src/triggerpi/board_init.cc
@@ -835,12 +835,9 @@ int board_init(void)
 	uint8_t ch_num;
 	int32_t iTemp;
 	uint8_t buf[3];
-//     if (!bcm2835_init())
-//         return 1;
-    bcm2835_spi_begin();
-    bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_LSBFIRST );     // The default
-    bcm2835_spi_setDataMode(BCM2835_SPI_MODE1);                   // The default
-    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_1024); // The default
+//    bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_LSBFIRST );     // The default
+//    bcm2835_spi_setDataMode(BCM2835_SPI_MODE1);                   // The default
+//    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_1024); // The default
     bcm2835_gpio_fsel(SPICS, BCM2835_GPIO_FSEL_OUTP);//
     bcm2835_gpio_write(SPICS, HIGH);
     bcm2835_gpio_fsel(DRDY, BCM2835_GPIO_FSEL_INPT);
@@ -898,8 +895,6 @@ int board_init(void)
 			printf("\33[%dA", (int)ch_num);
 		bsp_DelayUS(100000);
 			}
-    bcm2835_spi_end();
-    bcm2835_close();
 
     return 0;
 }
